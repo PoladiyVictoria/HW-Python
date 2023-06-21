312ac0611b593307f27ec5763699f622855b8f70
PoladiyVictoria <vikapoladiy@gmail.com>
2023-06-21 19:26:39 +0300

    Задача 38: Добавлен режим удаления

--- Task38/task38.py
+++ Task38/task38.py
@@ -79,15 +79,38 @@ def Change_phonebook():
                     f_change1.writelines('\n')
                     f_change1.writelines(person)
 
+def Remove_from_phonebook():
+    with open('E:\Домашка\HW Python\Task38\phonebook.txt', 'r', encoding='utf-8') as f_rem:
+        book = f_rem.read().splitlines()
+        human = input('Введите фамилию, имя или отчество: ')
+        for person in book:
+            some_man = person.split(':')
+            buf = some_man[0]
+            if buf == human:
+                book.remove(person)
+            else:
+                name = buf.split()
+                for partname in name:
+                    if partname == human:
+                        book.remove(person)
+        with open('E:\Домашка\HW Python\Task38\phonebook.txt', 'w', encoding='utf-8') as f_rem1:
+            for person in book:
+                if person == book[0]:
+                    f_rem1.writelines(person)
+                else:
+                    f_rem1.writelines('\n')
+                    f_rem1.writelines(person)
 
 print('Добро пожаловать в телефонный справочник!')
 print('Выберите режим работы со справочником:')
-mode = int(input('1 - чтение, 2 - запись, 3 - изменение: '))
+mode = int(input('1 - чтение, 2 - запись, 3 - изменение, 4 - удаление: '))
 if mode == 1:
     Read_phonebook()
 elif mode == 2:
     Entry_phonebook()
 elif mode == 3:
     Change_phonebook()
+elif mode == 4:
+    Remove_from_phonebook()
 else:
     print('Не корректный режим')
